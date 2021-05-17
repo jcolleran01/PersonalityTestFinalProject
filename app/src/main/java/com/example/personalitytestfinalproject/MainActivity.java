79093eb1551ffb1598c16aeec8e6eeca156bdf51
jcolleran01 <joshuacolleran@hotmail.com>
2021-05-16 23:06:29 -0700

    First commit

--- app/src/main/java/com/example/personalitytestfinalproject/MainActivity.java
+++ app/src/main/java/com/example/personalitytestfinalproject/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.personalitytestfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mTrueButton;
+    private Button mFalseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeTop("Correct!");
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeTop ("Incorrect!");
+            }
+        });
+    }
+    public void makeTop(String toastDisplay){
+        Toast toast = Toast.makeText(MainActivity.this, toastDisplay, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 0, 200);
+        toast.show();
     }
 }
